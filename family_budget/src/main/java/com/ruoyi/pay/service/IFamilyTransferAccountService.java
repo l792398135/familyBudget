230558960328e0eb80dc38f616a7a0a4204b0e26
liuxw72 <liuxw72@meicloud.com>
2021-09-22 14:38:02 +0800

    转账

---
+++ family_budget/src/main/java/com/ruoyi/pay/service/IFamilyTransferAccountService.java
@@ -0,0 +1,61 @@
+package com.ruoyi.pay.service;
+
+import java.util.List;
+import com.ruoyi.pay.domain.FamilyTransferAccount;
+
+/**
+ * 转账Service接口
+ * 
+ * @author ruoyi
+ * @date 2021-09-22
+ */
+public interface IFamilyTransferAccountService 
+{
+    /**
+     * 查询转账
+     * 
+     * @param id 转账主键
+     * @return 转账
+     */
+    public FamilyTransferAccount selectFamilyTransferAccountById(Long id);
+
+    /**
+     * 查询转账列表
+     * 
+     * @param familyTransferAccount 转账
+     * @return 转账集合
+     */
+    public List<FamilyTransferAccount> selectFamilyTransferAccountList(FamilyTransferAccount familyTransferAccount);
+
+    /**
+     * 新增转账
+     * 
+     * @param familyTransferAccount 转账
+     * @return 结果
+     */
+    public int insertFamilyTransferAccount(FamilyTransferAccount familyTransferAccount);
+
+    /**
+     * 修改转账
+     * 
+     * @param familyTransferAccount 转账
+     * @return 结果
+     */
+    public int updateFamilyTransferAccount(FamilyTransferAccount familyTransferAccount);
+
+    /**
+     * 批量删除转账
+     * 
+     * @param ids 需要删除的转账主键集合
+     * @return 结果
+     */
+    public int deleteFamilyTransferAccountByIds(String ids);
+
+    /**
+     * 删除转账信息
+     * 
+     * @param id 转账主键
+     * @return 结果
+     */
+    public int deleteFamilyTransferAccountById(Long id);
+}
---
+++ family_budget/src/main/java/com/ruoyi/pay/service/impl/FamilyTransferAccountServiceImpl.java
@@ -0,0 +1,96 @@
+package com.ruoyi.pay.service.impl;
+
+import java.util.List;
+import com.ruoyi.common.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.ruoyi.pay.mapper.FamilyTransferAccountMapper;
+import com.ruoyi.pay.domain.FamilyTransferAccount;
+import com.ruoyi.pay.service.IFamilyTransferAccountService;
+import com.ruoyi.common.core.text.Convert;
+
+/**
+ * 转账Service业务层处理
+ * 
+ * @author ruoyi
+ * @date 2021-09-22
+ */
+@Service
+public class FamilyTransferAccountServiceImpl implements IFamilyTransferAccountService 
+{
+    @Autowired
+    private FamilyTransferAccountMapper familyTransferAccountMapper;
+
+    /**
+     * 查询转账
+     * 
+     * @param id 转账主键
+     * @return 转账
+     */
+    @Override
+    public FamilyTransferAccount selectFamilyTransferAccountById(Long id)
+    {
+        return familyTransferAccountMapper.selectFamilyTransferAccountById(id);
+    }
+
+    /**
+     * 查询转账列表
+     * 
+     * @param familyTransferAccount 转账
+     * @return 转账
+     */
+    @Override
+    public List<FamilyTransferAccount> selectFamilyTransferAccountList(FamilyTransferAccount familyTransferAccount)
+    {
+        return familyTransferAccountMapper.selectFamilyTransferAccountList(familyTransferAccount);
+    }
+
+    /**
+     * 新增转账
+     * 
+     * @param familyTransferAccount 转账
+     * @return 结果
+     */
+    @Override
+    public int insertFamilyTransferAccount(FamilyTransferAccount familyTransferAccount)
+    {
+        familyTransferAccount.setCreateTime(DateUtils.getNowDate());
+        return familyTransferAccountMapper.insertFamilyTransferAccount(familyTransferAccount);
+    }
+
+    /**
+     * 修改转账
+     * 
+     * @param familyTransferAccount 转账
+     * @return 结果
+     */
+    @Override
+    public int updateFamilyTransferAccount(FamilyTransferAccount familyTransferAccount)
+    {
+        return familyTransferAccountMapper.updateFamilyTransferAccount(familyTransferAccount);
+    }
+
+    /**
+     * 批量删除转账
+     * 
+     * @param ids 需要删除的转账主键
+     * @return 结果
+     */
+    @Override
+    public int deleteFamilyTransferAccountByIds(String ids)
+    {
+        return familyTransferAccountMapper.deleteFamilyTransferAccountByIds(Convert.toStrArray(ids));
+    }
+
+    /**
+     * 删除转账信息
+     * 
+     * @param id 转账主键
+     * @return 结果
+     */
+    @Override
+    public int deleteFamilyTransferAccountById(Long id)
+    {
+        return familyTransferAccountMapper.deleteFamilyTransferAccountById(id);
+    }
+}
